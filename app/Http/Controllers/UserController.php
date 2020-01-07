34034044054cf4d2285e65f388c0fe2b7890b37c
Arief Rahman Y <Aryusniardi15@gmail.com>
2020-01-08 01:42:58 +0700

    1.0.0.1
    Update Validasi UserController
    PUNTEN ANYING AING LIEUR

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,8 +4,15 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use SimpleXMLElement;
 
 class UserController extends Controller {
+	/**
+    * Store a newly created resource in storage
+    * 
+    * @param \Illuminate\Http\Request $request
+    * @return \Illuminate\Http\Response
+    */
 	public function index(Request $request){
 		$acceptHeader = $request->header('Accept');
 
@@ -30,31 +37,103 @@ class UserController extends Controller {
                 }
                 return $xml->asXML();
             }
-
 		}
 		else {
 			return response('not acceptable!', 406);
 		}
 	}
 
+	/**
+	* Store a newly created resource in storage
+	* 
+	* @param \Illuminate\Http\Request $request
+	* @return \Illuminate\Http\Response
+	*/
 	public function store(Request $request){
-		$input = $request->all();
-		$user = User::create($input);
+		$acceptHeader = $request->header('accept');
+		// Hanya memvalidasi application/json atau application/xml
+		if ($acceptHeader === 'application/json') {
+			$contentTypeHeader = $request->header('Content-Type');
+			// Hanya application/json yang valid
+			if ($contentTypeHeader === 'application/json') {
+				$input = $request->all();
+				
+				$user = User::create($input);
 
-		return response()->json($user, 200);
-	}
+				return response()->json($user, 200);
+			} else {
+				return response('Unsupported Media Type', 403);
+			}
+		} else if ($acceptHeader === 'application/xml') {
+			$contentTypeHeader = $request->header('Content-Type');
+			// Hanya application/xml yang valid
+			if ($contentTypeHeader === 'application/xml') {
+				$input = $request->all();
 
-	public function show($id){
-		$user = User::find($id);
+				$user = User::create($input);
 
-		if (!$user) {
-			abort(404);
+				// Response XML
+				// Create XML User Element
+				$xml = new \SimpleXMLElement('<User/>');
+				// Changing every field user to XML Format
+				$xml->AddChild('id', $user->id);
+				$xml->AddChild('nik', $user->nik);
+				$xml->AddChild('nama', $user->nama);
+				$xml->AddChild('email', $user->email);
+				$xml->AddChild('password', $user->password);
+				$xml->AddChild('created_at', $user->created_at);
+				$xml->AddChild('updated_at', $user->updated_at);
+				
+				return $xml->asXML();
+			} else {
+				return response('Unsupported Media Type', 403);
+			}
+		} else {
+			return response('Not Acceptable!', 406);
 		}
-
-		return response()->json($user, 200);
 	}
 
+	/**
+	* Display the specified resource
+	* 
+	* @param int $id
+	* @return \Illuminate\Http\Response
+	*/
+	public function show(Request $request, $id){
+		$acceptHeader = $request->header('Accept');
+
+		// Validasi hanya application/json atau application/xml
+		$user = User::Find($id);
+		if ($acceptHeader === 'application/json') {
+			// Response JSON
+			return response()->json($user, 200);			
+		} else if ($acceptHeader === 'application/xml') {
+			// Response XML
+			$xml = new \SimpleXMLElement('<post/>');
+
+			$xml->addChild('id', $user->id);
+			$xml->AddChild('nik', $user->nik);
+			$xml->AddChild('nama', $user->nama);
+			$xml->AddChild('email', $user->email);
+			$xml->AddChild('password', $user->password);
+			$xml->AddChild('created_at', $user->created_at);
+			$xml->AddChild('updated_at', $user->updated_at);
+
+            return $xml->asXML();
+		} else {
+			return response('Not Acceptable!', 406);
+		}
+	}
+
+	/**
+	* Update the specified resource in storage
+	* 
+	* @param \Illuminate\Http\Request $request
+	* @param int $id
+	* @return \Illuminate\Http\Response
+	*/
 	public function update(Request $request, $id){
+		$acceptHeader = $request->hedaer('Accept');
 		$input = $request->all();
 
 		$user = User::find($id);
@@ -62,13 +141,55 @@ class UserController extends Controller {
 		if (!$user) {
 			abort(404);
 		}
-		$user->fill($input);
-		$user->save();
 
-		return response()->json($user,200);
+		// Validasi hanya application/json atau application/xml
+		if ($acceptHeader === 'application/json') {
+			// Hanya memvalidasi application/json
+			$contentTypeHeader = $request->header('Content-Type');
+			if ($contentTypeHeader === 'application/json') {
+				$user->fill($input);
+				$user->save();
+		
+				return response()->json($user,200);
+			} else {
+				return response('Unsupported Media Type', 403);
+			}
+		} else if ($acceptHeader === 'application/xml') {
+			// Hanya memvalidasi applicaion/xml
+			$contentTypeHeader = $request->header('Content-Type');
+			if ($contentTypeHeader === 'application/json') {
+				$input = $request->all();
+
+				$user->fill($input);
+				$user->save();
+
+				$xml = new \SimpleXMLElement('<User/>');
+				// Changing every field User to XML Format
+
+				$xml->addChild('id', $user->id);
+				$xml->AddChild('nik', $user->nik);
+				$xml->AddChild('nama', $user->nama);
+				$xml->AddChild('email', $user->email);
+				$xml->AddChild('password', $user->password);
+				$xml->AddChild('created_at', $user->created_at);
+				$xml->AddChild('updated_at', $user->updated_at);
+
+				return $xml->asXML();
+			} else {
+				return response('Unsupported Media Type', 403);
+			}
+		} else {
+			return response('Not Acceptable!', 406);
+		}
 	}
 
-	public function destroy($id){
+	/**
+	* Remove the specified resource from storage
+	* 
+	* @param int $id
+	* @return \Illuminate\Http\Response
+	*/
+	public function destroy(Request $request, $id){
 		$user = User::find($id);
 
 		if (!$user) {
